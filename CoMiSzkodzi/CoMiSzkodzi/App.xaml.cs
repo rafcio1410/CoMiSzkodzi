2bdcdcc998c8e68bf2a451c517a9b0f5c77b91e1
rafcio1410 <45153068+rafcio1410@users.noreply.github.com>
2018-11-18 23:26:15 +0100

    initiale

---
+++ CoMiSzkodzi/CoMiSzkodzi/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FreshMvvm;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CoMiSzkodzi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var page = FreshPageModelResolver.ResolvePageModel<PodsumujPosilkiPageModel>();
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ CoMiSzkodzi/CoMiSzkodzi/CoMiSzkodziBasePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using FreshMvvm;
+using Xamarin.Forms.Xaml;
+
+namespace CoMiSzkodzi
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CoMiSzkodziBasePage : ContentView
+	{
+    
+		public CoMiSzkodziBasePage ()
+		{
+			InitializeComponent ();
+            //TitleLabel.Text = Tytul;
+		}
+
+       
+    
+    }
+}
