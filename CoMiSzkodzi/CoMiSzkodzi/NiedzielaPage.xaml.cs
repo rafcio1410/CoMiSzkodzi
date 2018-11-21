3ab2de729b167a55bcaa2bc13ab2fc781ca147ef
rafcio1410 <45153068+rafcio1410@users.noreply.github.com>
2018-11-21 22:45:10 +0100

    nowe stronki plus dodanie png

---
+++ CoMiSzkodzi/CoMiSzkodzi/NiedzielaPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoMiSzkodzi
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NiedzielaPage : ContentPage
+	{
+		public NiedzielaPage ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+	}
+}
---
+++ CoMiSzkodzi/CoMiSzkodzi/WtorekPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoMiSzkodzi
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WtorekPage : ContentPage
+	{
+		public WtorekPage ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+	}
+}
