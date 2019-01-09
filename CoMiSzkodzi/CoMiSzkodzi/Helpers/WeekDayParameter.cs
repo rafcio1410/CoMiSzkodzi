28e7c6aa21685f08cd241f0bece30278822ef884
Perry89 <39899619+Perry89@users.noreply.github.com>
2019-01-09 20:38:51 +0000

    Revert "Revert "Merge branch 'master' into Siwy""
    This reverts commit 814e7b06c0e33a49889fa6d657863431150a55e0.

---
+++ CoMiSzkodzi/CoMiSzkodzi/Helpers/WeekDayParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoMiSzkodzi.Enums;
+
+namespace CoMiSzkodzi.Helpers
+{
+    public class WeekDayParameter
+    {
+        public WeekDayParameter()
+        {
+        }
+        public WeekDays DayOfTheWeek
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CoMiSzkodzi/CoMiSzkodzi/Pages/AddNewFoodPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoMiSzkodzi.Pages
+{
+    public partial class AddNewFoodPage : ContentPage
+    {
+        public AddNewFoodPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
